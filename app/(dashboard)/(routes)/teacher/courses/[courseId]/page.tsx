416ec441deb7322dcddad3a19d663b43d9371aec
Beamlak996 <beamlaktsegaye996@gmail.com>
2023-12-14 06:16:32 -0900

    Course edit page ui

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,8 +1,61 @@
+import { IconBadge } from "@/components/icon-badge"
+import { db } from "@/lib/db"
+import { auth } from "@clerk/nextjs"
+import { LayoutDashboard } from "lucide-react"
+import { redirect } from "next/navigation"
 
-const CourseIdPage = ({params}: {params: {courseId: string}}) => {
+const CourseIdPage = async ({params}: {params: {courseId: string}}) => {
+  const userId = auth()
+
+  if(!userId) {
+    return redirect("/")
+  }
+
+  const course = await db.course.findUnique({
+    where: {
+        id: params.courseId,
+    }
+  })
+
+  if(!course) {
+    return redirect("/");
+  }
+
+  const requiredFields = [
+    course.title,
+    course.description,
+    course.imageUrl,
+    course.price,
+    course.categoryId
+  ]
+
+  const totalFields = requiredFields.length
+  const completedFileds = requiredFields.filter(Boolean).length
+
+  const completionText = `(${completedFileds}/${totalFields})`
+
   return (
-    <div>
-        Course ID: {params.courseId}
+    <div className="p-6" >
+        <div className="flex items-center justify-between" >
+            <div className="flex flex-col gap-y-2" >
+                <h1 className="text-2xl font-medium" >
+                    Course Setup
+                </h1>
+                <span className="text-sm text-slate-700" >
+                    Complete all fields {completionText}
+                </span>
+            </div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16" >
+            <div>
+                <div className="flex items-center gap-x-2" >
+                    <IconBadge icon={LayoutDashboard} />
+                    <h2 className="text-xl" >
+                        Customize your course
+                    </h2>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
