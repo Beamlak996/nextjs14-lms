c661df3d0e404944aab8e3b27755b73b528ca1a5
Beamlak996 <beamlaktsegaye996@gmail.com>
2023-12-15 06:23:54 -0900

    Chapter reorder done

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/chapters-form.tsx
@@ -1,11 +1,15 @@
 "use client";
-import * as z from "zod";
-import { useForm } from "react-hook-form";
 
+import * as z from "zod";
+import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Button } from "@/components/ui/button";
-import { Pencil, PlusCircle } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { Loader2, PlusCircle } from "lucide-react";
 import { useState } from "react";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import { Chapter, Course } from "@prisma/client";
+
 import {
   Form,
   FormControl,
@@ -13,53 +17,78 @@ import {
   FormItem,
   FormMessage,
 } from "@/components/ui/form";
-import toast from "react-hot-toast";
-import axios from "axios";
-import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
-import { Chapter, Course } from "@prisma/client";
 import { Input } from "@/components/ui/input";
 
-type ChaptersFormProps = {
-  initialData: Course & { chapters: Chapter[] }
+import { ChaptersList } from "./chapters-list";
+
+interface ChaptersFormProps {
+  initialData: Course & { chapters: Chapter[] };
   courseId: string;
-};
+}
 
 const formSchema = z.object({
-  title: z.string().min(1)
+  title: z.string().min(1),
 });
 
 export const ChaptersForm = ({ initialData, courseId }: ChaptersFormProps) => {
-  const [isCreating, setIsCreating] = useState(false)
+  const [isCreating, setIsCreating] = useState(false);
   const [isUpdating, setIsUpdating] = useState(false);
+
+  const toggleCreating = () => {
+    setIsCreating((current) => !current);
+  };
 
   const router = useRouter();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      title: ""
+      title: "",
     },
   });
 
   const { isSubmitting, isValid } = form.formState;
 
-  const toggleCreating = () => setIsCreating((current) => !current);
-
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
       await axios.post(`/api/courses/${courseId}/chapters`, values);
-      toast.success(`Chapter created`);
-      router.refresh();
-    } catch (error) {
-      toast.error("Something went wrong");
-    } finally {
+      toast.success("Chapter created");
       toggleCreating();
+      router.refresh();
+    } catch {
+      toast.error("Something went wrong");
     }
   };
 
+  const onReorder = async (updateData: { id: string; position: number }[]) => {
+    try {
+      setIsUpdating(true);
+
+      await axios.put(`/api/courses/${courseId}/chapters/reorder`, {
+        list: updateData,
+      });
+      toast.success("Chapters reordered");
+      router.refresh();
+    } catch {
+      toast.error("Something went wrong");
+    } finally {
+      setIsUpdating(false);
+    }
+  };
+
+  const onEdit = (id: string) => {
+    router.push(`/teacher/courses/${courseId}/chapters/${id}`);
+  };
+
   return (
-    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+    <div className="relative mt-6 border bg-slate-100 rounded-md p-4">
+      {isUpdating && (
+        <div className="absolute h-full w-full bg-slate-500/20 top-0 right-0 rounded-m flex items-center justify-center">
+          <Loader2 className="animate-spin h-6 w-6 text-sky-700" />
+        </div>
+      )}
       <div className="font-medium flex items-center justify-between">
         Course chapters
         <Button onClick={toggleCreating} variant="ghost">
@@ -95,15 +124,25 @@ export const ChaptersForm = ({ initialData, courseId }: ChaptersFormProps) => {
                 </FormItem>
               )}
             />
-            <Button disabled={!isValid || isSubmitting}>Create</Button>
+            <Button disabled={!isValid || isSubmitting} type="submit">
+              Create
+            </Button>
           </form>
         </Form>
       )}
-
       {!isCreating && (
-        <div className={cn("text-sm mt-2", !initialData.chapters.length && "text-slate-500 italic")} >
+        <div
+          className={cn(
+            "text-sm mt-2",
+            !initialData.chapters.length && "text-slate-500 italic"
+          )}
+        >
           {!initialData.chapters.length && "No chapters"}
-          {/* TODO: Add chapters */}
+          <ChaptersList
+            onEdit={onEdit}
+            onReorder={onReorder}
+            items={initialData.chapters || []}
+          />
         </div>
       )}
       {!isCreating && (
--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -1,30 +1,53 @@
-import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
-export async function POST(req: Request) {
-    try {
-        const { userId } = auth()
-        const { title } = await req.json()
+import { db } from "@/lib/db";
 
-        if(!userId) {
-            return new NextResponse("Unauthorized", { status: 401 })
-        }
+export async function POST(
+  req: Request,
+  { params }: { params: { courseId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const { title } = await req.json();
 
-        if(!title) {
-            return new NextResponse("Title is required", { status: 400 })
-        }
-
-        const course = await db.course.create({
-            data: {
-                userId,
-                title
-            }
-        })
-
-        return NextResponse.json({course})
-    } catch (error) {
-        console.log("[COURSES_POST]", error)
-        return new NextResponse("Internal error", { status: 500 })
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
     }
+
+    const courseOwner = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId: userId,
+      },
+    });
+
+    if (!courseOwner) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const lastChapter = await db.chapter.findFirst({
+      where: {
+        courseId: params.courseId,
+      },
+      orderBy: {
+        position: "desc",
+      },
+    });
+
+    const newPosition = lastChapter ? lastChapter.position + 1 : 1;
+
+    const chapter = await db.chapter.create({
+      data: {
+        title,
+        courseId: params.courseId,
+        position: newPosition,
+      },
+    });
+
+    return NextResponse.json(chapter);
+  } catch (error) {
+    console.log("[CHAPTERS]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
 }
