1a6b466ba9af4d64db3a569253bfee008efbb6c8
Beamlak996 <beamlaktsegaye996@gmail.com>
2023-12-16 05:50:01 -0900

    Course grid & Card UI Done

---
+++ actions/get-progress.tsx
@@ -0,0 +1,37 @@
+import { db } from "@/lib/db"
+
+export const getProgress = async (
+    userId: string,
+    courseId: string
+): Promise<number> => {
+    try {
+        const publishedChapters = await db.chapter.findMany({
+            where: {
+                courseId: courseId,
+                isPublished: true
+            },
+            select: {
+                id: true
+            }
+        })
+
+        const publishedChaptersIds = publishedChapters.map((chapter) => chapter.id)
+
+        const validCompletedChapters = await db.userProgress.count({
+            where: {
+                userId: userId,
+                chapterId: {
+                    in: publishedChaptersIds
+                },
+                isCompleted: true
+            }
+        })
+
+        const progressPercantage = (validCompletedChapters / publishedChaptersIds.length) * 100
+
+        return progressPercantage
+    } catch (error) {
+        console.log("[GET_PROGRESS]", error)
+        return 0
+    }
+}
