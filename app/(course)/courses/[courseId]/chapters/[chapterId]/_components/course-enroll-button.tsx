925649c1fd836a12117c873f97e0c247139df43d
Beamlak996 <beamlaktsegaye996@gmail.com>
2023-12-16 21:58:56 -0900

    Stripe Integration Complete

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-enroll-button.tsx
@@ -1,6 +1,9 @@
 "use client"
 import { Button } from "@/components/ui/button"
 import { formatPrice } from "@/lib/format"
+import axios from "axios"
+import { useState } from "react"
+import toast from "react-hot-toast"
 
 type CourseEnrollButtonProps = {
     courseId: string
@@ -8,8 +11,29 @@ type CourseEnrollButtonProps = {
 }
 
 export const CourseEnrollButton = ({courseId, price}: CourseEnrollButtonProps) => {
+    const [isLoading, setIsLoading] = useState(false) 
+
+    const onClick = async () => {
+        try {
+            setIsLoading(true)
+
+            const response = await axios.post(`/api/courses/${courseId}/checkout`)
+
+            window.location.assign(response.data.url)
+        } catch (error) {
+            toast.error("Something went wrong")
+        } finally {
+            setIsLoading(false)
+        }
+    }
+
     return (
-        <Button className="w-full md:w-auto" size="sm" >
+        <Button 
+          className="w-full md:w-auto" 
+          size="sm"
+          onClick={onClick}
+          disabled={isLoading} 
+        >
             Enroll for {formatPrice(price)}
         </Button>
     )
